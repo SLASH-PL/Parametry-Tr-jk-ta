85dd37a43f41d4124f615922e43ccd2597f7ce4e
Hubert Haładyna <hubert.haladyna@microsoft.wsei.edu.pl>
2023-10-22 14:21:34 +0200

    Actually made the damn thing work

--- Parametry Trójkąta/Program.cs
+++ Parametry Trójkąta/Program.cs
@@ -1,17 +1,97 @@
-﻿string input = Console.ReadLine();
+﻿using System.Security.Authentication.ExtendedProtection;
+using System.Globalization;
+
+CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo("en-US");
+string input = Console.ReadLine();
 string[] line = input.Split("; ");
-int a = int.Parse(line[0]);
-int b = int.Parse(line[1]);
-int c = int.Parse(line[2]);
+double a = double.Parse(line[0]);
+double b = double.Parse(line[1]);
+double c = double.Parse(line[2]);
+string typ = "";
 
 
-if(a < 0 || b <0 || c < 0)
+if ((check(a, b, c)) == 0)
 {
     Console.WriteLine("Błędne dane. Długości boków muszą być dodatnie!");
 }
-else
+else if ((check(a, b, c)) == 1)
 {
-    Console.WriteLine(line[0]);
-    Console.WriteLine(line[1]);
-    Console.WriteLine(line[2]);
+    Console.WriteLine("Błędne dane. Trójkąta nie można zbudować!");
+}
+else if ((check(a, b, c)) == 2)
+{
+    //obwód
+    Console.WriteLine($"obwód = {a + b + c}");
+    //pole
+    double s = (a + b + c) / 2;
+    double p = Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+    p = Math.Round(p, 2);
+    Console.WriteLine($"pole = {p}");
+    //typ
+    if (check1(a, b, c) == 0)
+    {
+        typ = "prostokątny";
+    }
+    else if (check1(a, b, c) == 1)
+    {
+        typ = "ostrokątny";
+    }
+    else if (check1(a, b, c) == 2)
+    {
+        typ = "rozwartokątny";
+    }
+    else
+    {
+        typ = "błędem w matrixie";
+    }
+    Console.WriteLine($"trójkąt jest {typ}");
+    //równoramienny/równoboczny?
+    if (a.Equals(b) && b.Equals(c) && c.Equals(a))
+    {
+        Console.WriteLine("trójkąt równoboczny");
+    }
+    else if (a.Equals(b) || b.Equals(c) || c.Equals(a))
+    {
+        Console.WriteLine("trójkąt równoramienny");
+    }
+}
+
+int check(double a, double b, double c)
+{
+    if(a <= 0 || b <= 0 || c <= 0)
+    {
+        return 0; //false
+    }
+    else if (a + b < c || a + c <= b || b + c <= a)
+    {
+        return 1; //false
+    }
+    else
+    {
+        return 2; //true
+    }
+}
+
+int check1(double a, double b, double c)
+{
+    if ((Math.Pow(a,2) == Math.Pow(b,2) + Math.Pow(c,2)) || (Math.Pow(b, 2) == Math.Pow(a, 2) + Math.Pow(c, 2)) || (Math.Pow(c, 2) == Math.Pow(a, 2) + Math.Pow(b, 2)))
+    {
+        return 0;
+        //prostokątny
+    }
+    else if ((Math.Pow(a, 2) > Math.Pow(b, 2) + Math.Pow(c, 2)) || (Math.Pow(b, 2) > Math.Pow(a, 2) + Math.Pow(c, 2)) || (Math.Pow(c, 2) > Math.Pow(a, 2) + Math.Pow(b, 2)))
+    {
+        return 2;
+        //rozwartokątny
+    }
+    else if ((Math.Pow(a, 2) < Math.Pow(b, 2) + Math.Pow(c, 2)) || (Math.Pow(b, 2) < Math.Pow(a, 2) + Math.Pow(c, 2)) || (Math.Pow(c, 2) < Math.Pow(a, 2) + Math.Pow(b, 2)))
+    {
+        return 1;
+        //ostrokątny
+    }
+    else
+    {
+         return 3;
+        //błąd w matrixie
+    }
 }
