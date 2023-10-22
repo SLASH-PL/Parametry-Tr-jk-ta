f20ab72fbba4daa6daadb02e77b52458abb050f5
Hubert Haładyna <hubert.haladyna@microsoft.wsei.edu.pl>
2023-10-22 12:08:27 +0200

    Add project files.

---
+++ Parametry Trójkąta/Program.cs
@@ -0,0 +1,17 @@
+﻿string input = Console.ReadLine();
+string[] line = input.Split("; ");
+int a = int.Parse(line[0]);
+int b = int.Parse(line[1]);
+int c = int.Parse(line[2]);
+
+
+if(a < 0 || b <0 || c < 0)
+{
+    Console.WriteLine("Błędne dane. Długości boków muszą być dodatnie!");
+}
+else
+{
+    Console.WriteLine(line[0]);
+    Console.WriteLine(line[1]);
+    Console.WriteLine(line[2]);
+}
